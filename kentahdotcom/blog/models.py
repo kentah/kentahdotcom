db7bd9142923adf3be4fbda21842a64019ffc033
kenthow@gmail.com <kentah@Kents-MacBook-Pro.local>
2018-01-25 17:24:57 -0600

    Created basic db model

--- kentahdotcom/blog/models.py
+++ kentahdotcom/blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=140, unique=True)
+    slug = models.SlugField(max_length=140, unique=True)
+    body = models.TextField()
+
+    posted = models.DateTimeField(db_index=True, auto_now_add=True, verbose_name=_('postDate'))
+    modified = models.DateTimeField(null=True, verbose_name=_('modified'))
+    category = models.ForeignKey('blog.Category', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name=_('post')
+        verbose_name_plural=_('posts')
+        ordering = ['-posted']
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=140, db_index=True)
+    slug = models.SlugField(max_length=140, db_index=True)
+
+    def __str__(self):
+        return self.title
--- kentahdotcom/kentahdotcom/settings.py
+++ kentahdotcom/kentahdotcom/settings.py
@@ -77,7 +77,6 @@ WSGI_APPLICATION = 'kentahdotcom.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': os.environ.get('DB_NAME', ''),
         'USER': os.environ.get('DB_USER', ''),
         'PASSWORD': os.environ.get('DB_PASS', ''),
@@ -125,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
