34ccc0c01afb7302cdcef7c513365caf07a8f105
kenthow@gmail.com <kentah@kents-mbp.attlocal.net>
2018-01-28 16:15:41 -0600

    Moved Procfile and requirements.txt up one level. Wired blog.urls to urls.py, class-based views

---
+++ kentahdotcom/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from blog import views
+
+
+urlpatterns = [
+    path('', views.Index.as_view(), name='index'),
+]
--- kentahdotcom/blog/views.py
+++ kentahdotcom/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
 
-# Create your views here.
+from .models import Blog, Category
+
+
+class Index(View):
+    model = Blog
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('It works')
