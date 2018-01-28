d90dff8a39dc8a05f4ed5a34b655a6590d3d8f4c
kenthow@gmail.com <kentah@kents-mbp.attlocal.net>
2018-01-28 17:57:01 -0600

    Added bootstrap and basic blog logic. Created Audio and Visual apps for galleries. Structured template dir for blog

--- kentahdotcom/blog/views.py
+++ kentahdotcom/blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import View, TemplateView, ListView
 
-from .models import Blog, Category
+from .models import Blog
 
 
-class Index(View):
+class Index(ListView):
     model = Blog
+    template_name = '../templates/blog/index.html'
 
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('It works')
+    def getTitleList(self, request, *args, **kwargs):
+        titles = self.get_queryset().latest('posted')
+        response = HttpResponse
+        return response
+
+
